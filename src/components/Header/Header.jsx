9f97191d90d8388c463477c4c9d09cd42603b9a0
Gurpreet Singh <gurpreetwork05@gmail.com>
2023-05-31 16:44:58 +0530

    UI Header Compeleted

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
 import './Header.scss';
+import SearchIcon from '../../assets/icons/search.png';
+import Heart from '../../assets/icons/heart.png';
+import Profile from '../../assets/icons/profile.png';
+import Shopping from '../../assets/icons/shopping.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-     return <div>Header</div>;
+     return (
+          <>
+               <div className="nav-bar-container">
+                    <div className="left-container">
+                         <div className="logo">
+                              <b>Exclusive</b>
+                         </div>
+                         <div className="navigation-bar">
+                              <Link href="#">Home</Link>
+                              <Link href="#">Contact</Link>
+                              <Link href="#">About</Link>
+                              <Link href="#">Sign Up</Link>
+                         </div>
+                    </div>
+                    <div className="right-container">
+                         <div className="seach-container">
+                              <input
+                                   type="text"
+                                   placeholder="What are you looking for?"
+                              />
+                              <img src={SearchIcon} />
+                         </div>
+                         <div className="header-icon-container">
+                              <img src={Heart} />
+                              <img src={Profile} />
+                              <img src={Shopping} />
+                         </div>
+                    </div>
+               </div>
+          </>
+     );
 };
 
 export default Header;
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
 import './TopBar.scss';
 import DropDown from '../DropDown/DropDown';
 import { Link } from 'react-router-dom';
 import { FaAngleDown } from 'react-icons/fa';
+
 const TopBar = () => {
      return (
           <>
-               <div className="topBar-container">
-                    <div className="empty"></div>
-                    <div className="topBar-text">
+               <div className="top-bar-container">
+                    <div></div>
+                    <div className="top-bar-text">
                          Summer Sale For All Swim Suits And Free Express
                          Delivery - OFF 50%!
-                         <Link href="#" className="link">
+                         <Link href="#">
                               <b>ShopNow</b>
                          </Link>
                     </div>
 
                     <div className="drop-dowm">
                          <DropDown />
-                         <FaAngleDown className="icon" />
+                         <FaAngleDown />
                     </div>
                </div>
           </>
